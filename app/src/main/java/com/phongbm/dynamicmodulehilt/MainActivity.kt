e9ef564bcdf46020f11d51885d80d7bf5aff4741
PhongBM <phongbm.it@gmail.com>
2021-08-23 21:13:23 +0700

    Project: initial

---
+++ app/src/main/java/com/phongbm/dynamicmodulehilt/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.phongbm.dynamicmodulehilt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.phongbm.dynamicmodulehilt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/phongbm/dynamicmodulehilt/OneFragment.kt
@@ -0,0 +1,25 @@
+package com.phongbm.dynamicmodulehilt
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.phongbm.dynamicmodulehilt.databinding.FragmentOneBinding
+
+class OneFragment : Fragment() {
+    private var _binding: FragmentOneBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentOneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
